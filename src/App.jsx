8702483f0c143f90c2acf80587dc4786fef9a6a9
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2023-08-04 20:57:52 +0500

    task 2 done with the edit option

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,81 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [value , setvalue] = useState(' ')
+const [value , setvalue] = useState('')
   
-  const [value2 , setvalue2] = useState([])
+const [value2 , setvalue2] = useState([])
+  
+const [ toglebtn , settoglebtn] = useState(true)
+
+const [edititem , setedititem ] = useState(null) 
  
     function  handlechange(event)
   {
-    setvalue(event.target.value) ; 
+    let aa = event.target.value
+    setvalue(aa) ; 
+  
+    
   }
-  function  handlebutton(){
+  function  addtaskbutton(){
         
-    setvalue2([ value , ...value2  ])
+   if(!value) 
+   { 
+    alert("inputBar is empty")
+   }
+   else if(value && !toglebtn){
+           
+          setvalue2( 
+            value2.map(
+              (elem)=>
+              {
+               if (elem.id === edititem )
+               {
+                return {...elem , name :value }
+               }
+                 return elem ; 
+              }
+            )
+           )
+           setvalue("") ; 
+           settoglebtn(true)
+   }
+else
+{
+  const inputdata = {id: new Date().getTime().toString() , name:value }
+  setvalue2([ inputdata , ...value2  ])
+  setvalue("")
+
+}
+
   } 
   function handleremove(index)
   {
-     setvalue2((prelist) =>{
-      const updatelist = [...prelist] ;
-      console.log(updatelist)
-      updatelist.splice(index ,1 ) 
+    const updatearray = value2.filter(
+      (elem)=>
+      {
+        return index !== elem.id 
+      }
+    );
+    setvalue2(updatearray) ; 
 
-      return updatelist ; 
-     })
+  }
+  function handleedit(index)
+  {
+      let updateitem =  value2.find((elem)=> 
+      {
+         return  elem.id === index
+      })
+
+      console.log(updateitem.id)   ;  
+      
+      settoglebtn(false) 
+     
+      setvalue(updateitem.name)
+
+      setedititem(index)
+
+
+
   }
   return (
     <div className="App">
@@ -34,22 +88,27 @@ function App() {
         <div className='input'>
             <input type="text"
             onChange={handlechange}
+            value={value}
                />
-               <button onClick={handlebutton}>Add task </button>
+
+            {
+              toglebtn ? <button onClick={addtaskbutton} className='btn'>Add task </button> :<button className='btn' onClick={addtaskbutton}>edit </button>
+            }   
+               
          
             
 
         </div>
-            <ul>
+            <ul className='listitems'>
               {
-                value2.map((items ,  index)=>
+                value2.map((items)=>
                { return(
                   <div className='itemsinlist'>
                   
-                  <li key={index}> {items}</li>
-                  <button onClick={() => handleremove(index)}  className='btn'>remove</button>
-                      
-                  </div>
+                  <li key={items.id} >   {items.name}</li>
+                  <button onClick={() => handleremove(items.id)}  className='btn'>remove</button>
+                  <button onClick={()=>handleedit(items.id)} className='btn'>edit</button>
+                  </div>  
                     
                 )} )
               }
